6583a634dca6635eb65ca34d95a487153d4f5659
Ashish Singh <ashish.singh@vegavid.com>
2022-08-26 19:14:54 +0530

    first commit

---
+++ programs/anchorFirst/src/lib.rs
@@ -0,0 +1,30 @@
+use anchor_lang::prelude::*;
+
+declare_id!("nAAoDaKw9XQF9QgpyT6RyTvHPhdwd1fqkPZTM8LiW7H");
+
+#[program]
+pub mod anchor_first {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>,_bump:u8) -> Result<()> {
+        msg!("heeeeeeeeeeeeeee");
+        print!("hello");
+
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+#[instruction(_bump : u8)]
+pub struct Initialize<'info>{
+    #[account(init,seeds=[owner.key.as_ref()],bump,payer=owner,space=100)]
+    pub account_user:Account<'info,AccountUser>,
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    pub system_program: Program<'info,System>
+}
+
+#[account]
+pub struct AccountUser {
+    pub user: String,
+        }
